2b6f7f8b690d583d1f652f0ceaab7f109830d611
Jonathan Schemoul <jonathan.schemoul@gmail.com>
2016-11-30 22:14:16 +0100

    adding classifiers

--- setup.py
+++ setup.py
@@ -8,7 +8,14 @@ setup(name='mihome',
       description="Xiaomi Mi Home Python bindings",
       long_description="""\
 A library to receive paquets, events and to control the Xiaomi Mi Home hub and other devices""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 3",
+          "Intended Audience :: Developers",
+          "Intended Audience :: System Administrators",
+          "Topic :: System :: Hardware"
+      ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='xiaomi udp paquet iot mi home aqara',
       author='Jonathan Schemoul',
       author_email='jonathan.schemoul@gmail.com',
