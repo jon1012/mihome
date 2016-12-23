c5718367ddd02b18085a8e9a61836789963c3e55
illxi <illxi@msn.com>
2016-12-23 21:06:35 +0800

    listen to mqtt and turn things on/off

---
+++ example_code/mihome_mqtt.py
@@ -0,0 +1,146 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from mihome import *
+import ConfigParser
+import paho.mqtt.client as mqtt
+import json
+import sys
+import thread
+from Crypto.Cipher import AES
+import binascii
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+subscribed_channels = set()
+
+IV = bytearray([0x17, 0x99, 0x6d, 0x09, 0x3d, 0x28, 0xdd, 0xb3, 0xba, 0x69, 0x5a, 0x2e, 0x6f, 0x58, 0x56, 0x2e])
+
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, rc):
+    print("Connected with result code "+str(rc))
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    #client.subscribe("xiaomi/test")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print "Topic: ", msg.topic+'\nMessage: '+str(msg.payload)
+    items = msg.topic.split('/')
+    device_model = items[1]
+    device_name = items[2]
+    sid = items[3]
+    device_channel = items[4]
+    gateway_address = userdata[1][sid]['gateway_address']
+    token = userdata[0][userdata[1][sid]['gateway_sid']]
+
+    print userdata[2]
+    print type(userdata[2])
+    user_key = userdata[2][str(gateway_address)]
+
+    print "userkey:", user_key
+
+    aes = AES.new(user_key, AES.MODE_CBC, str(IV))
+
+    ciphertext = aes.encrypt(token)
+
+    write_key = binascii.hexlify(ciphertext)
+    if msg.payload == "1":
+        command = "on"
+    elif msg.payload == "0":
+        command = "off"
+    else:
+        command = "unknown"
+
+    write_command = {"cmd":"write",
+                     "model":device_model,
+                     "sid":sid,
+                     "short_id":4343,
+                     "data":{device_channel:command,"key":write_key}}
+    userdata[3].send_command(write_command, gateway_address, 9898)
+    print write_command
+
+
+
+
+def prepare_mqtt(MQTT_SERVER, MQTT_PORT=1883):
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect(MQTT_SERVER, MQTT_PORT, 60)
+    return client
+
+def push_data(client, model, sid, cmd, data, id2name, PATH_FMT):
+    for key, value in data.items():
+        if sid in id2name:
+            dname = id2name[sid].decode('utf8')
+        else:
+            dname = sid
+        path = PATH_FMT.format(model=model,
+                             name=dname,
+                             sid=sid,
+                             cmd=cmd,
+                             prop=key)
+        client.publish(path, payload=value.upper(), qos=0)
+
+        if model in ['plug', 'ctrl_neutral2', 'ctrl_neutral1']:
+            if path in subscribed_channels:
+                pass
+            else:
+                print "Subscribe to", path + "/command"
+                client.subscribe(path + "/command")
+                subscribed_channels.add(path)
+
+def ConfigSectionMap(Config, section):
+    dict1 = {}
+    options = Config.options(section)
+    for option in options:
+        try:
+            dict1[option] = Config.get(section, option)
+            if dict1[option] == -1:
+                print("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
+
+
+
+if __name__ == "__main__":
+    id2name = dict()
+    Config = ConfigParser.ConfigParser()
+    Config.read("mihome_mqtt.ini")
+    MQTT_SERVER = ConfigSectionMap(Config, "MQTT")['server']
+    MQTT_PORT = ConfigSectionMap(Config, "MQTT")['port']
+    PATH_FMT =  ConfigSectionMap(Config, "MQTT")['mqtt_path']
+    devices = ConfigSectionMap(Config, "devices")['sub_devices']
+
+    user_key = Config._sections['user_key']
+    data = json.loads(devices.decode('gbk'))
+    for i in data:
+        id2name[i['did'].split('.')[1]] = i['name']
+    client = prepare_mqtt(MQTT_SERVER, MQTT_PORT)
+    #model, sid, cmd, data, gateway
+    cb = lambda m, s, c, d: push_data(client, m, s, c, d, id2name, PATH_FMT)
+    connector = XiaomiConnector(data_callback=cb)
+    node_list = connector.get_nodes()
+    #print node_list
+    #p = connector.send_whois()
+
+    for node in node_list:
+        if node_list[node]["model"] == "gateway":
+            pass
+        else:
+            connector.request_current_status(node, node_list[node]['gateway_address'])
+    try:
+        thread.start_new_thread(client.loop_forever, ())
+
+    except:
+        print "Error: unable to start thread"
+    print "start listenning"
+    while True:
+        connector.check_incoming()
+
+        client.user_data_set((connector.get_token(),node_list,user_key,connector))
+        #print connector.get_token()
--- mihome/connector.py
+++ mihome/connector.py
@@ -1,9 +1,8 @@
 from past.builtins import basestring
 import socket
-import binascii
 import struct
 import json
-
+import datetime
 
 class XiaomiConnector:
     """Connector for the Xiaomi Mi Hub and devices on multicast."""
@@ -19,8 +18,10 @@ class XiaomiConnector:
         self.data_callback = data_callback
         self.last_tokens = dict()
         self.socket = self._prepare_socket()
-
         self.nodes = dict()
+        self.gnodes = dict()
+        self.gateways = set()
+        self.send_whois()
 
     def _prepare_socket(self):
         sock = socket.socket(socket.AF_INET,  # Internet
@@ -43,7 +44,8 @@ class XiaomiConnector:
         data, addr = self.socket.recvfrom(self.SOCKET_BUFSIZE)
         try:
             payload = json.loads(data.decode("utf-8"))
-            print(payload)
+            #print addr, ": ", payload
+            self.gateway_address = addr[0]
             self.handle_incoming_data(payload)
 
         except Exception as e:
@@ -56,45 +58,145 @@ class XiaomiConnector:
         """
         if isinstance(payload.get('data', None), basestring):
             cmd = payload["cmd"]
+
             if cmd in ["heartbeat", "report", "read_ack"]:
                 if self.data_callback is not None:
-                    self.data_callback(payload["model"],
-                                       payload["sid"],
-                                       payload["cmd"],
-                                       json.loads(payload["data"]))
+                    try:
+                        self.data_callback(payload["model"],
+                                           payload["sid"],
+                                           payload["cmd"],
+                                           json.loads(payload["data"]))
+                    except KeyError, e:
+                        print "KeyError", e
 
-            if cmd == "read_ack" and payload["sid"] not in self.nodes:
-                self.nodes[payload["sid"]] = dict(model=payload["model"])
+
 
             if cmd == "heartbeat" and payload["sid"] not in self.nodes:
-                self.request_sids(payload["sid"])
+                #self.request_sids(payload["sid"])
                 self.nodes[payload["sid"]] = json.loads(payload["data"])
                 self.nodes[payload["sid"]]["model"] = payload["model"]
                 self.nodes[payload["sid"]]["sensors"] = []
-
-            if cmd == "get_id_list_ack":
-                device_sids = json.loads(payload["data"])
-                self.nodes[payload["sid"]]["nodes"] = device_sids
-
-                for sid in device_sids:
-                    self.request_current_status(sid)
+                self.nodes[payload["sid"]]["gateway_address"] = self.gateway_address
 
         if "token" in payload:
             self.last_tokens[payload["sid"]] = payload['token']
 
-    def request_sids(self, sid):
+    def request_sids(self, sid, ip):
         """Request System Ids from the hub."""
-        self.send_command({"cmd": "get_id_list", sid: sid})
+        self.send_command({"cmd": "get_id_list", sid: sid}, ip)
 
-    def request_current_status(self, device_sid):
+    def request_current_status(self, device_sid, ip):
         """Request (read) the current status of the given device sid."""
-        self.send_command({"cmd": "read", "sid": device_sid})
+        self.send_command({"cmd": "read", "sid": device_sid}, ip)
 
-    def send_command(self, data):
+    def send_command(self, data, addr = MULTICAST_ADDRESS, port=MULTICAST_PORT):
         """Send a command to the UDP subject (all related will answer)."""
-        self.socket.sendto(json.dumps(data).encode("utf-8"),
-                           (self.MULTICAST_ADDRESS, self.MULTICAST_PORT))
-
+        #self.socket.sendto(json.dumps(data).encode("utf-8"), (addr, self.MULTICAST_PORT))
+        if type(data) is dict:
+            self.socket.sendto(json.dumps(data).encode("utf-8"), (addr, port))
+            print "SEND dATA:", json.dumps(data).encode("utf-8")
+        else:
+            self.socket.sendto(data.encode("utf-8"), (addr, port))
+            print "SEND dATA:", data.encode("utf-8")
     def get_nodes(self):
         """Return the current discovered node configuration."""
         return self.nodes
+    def get_token(self):
+        return self.last_tokens
+
+    def send_whois(self):
+        self.socket.sendto(json.dumps({"cmd":"whois"}).encode("utf-8"),
+                           (self.MULTICAST_ADDRESS, self.SERVER_PORT))
+
+        endTime = datetime.datetime.now() + datetime.timedelta(seconds=5)
+        print "Waiting for replies (5s) ..."
+        while datetime.datetime.now() <= endTime:
+
+            data, addr = self.socket.recvfrom(self.SOCKET_BUFSIZE)
+            try:
+                payload = json.loads(data.decode("utf-8"))
+                #self.gateways.add(addr[0])
+
+                #self.gateway_address = addr[0]
+
+            except Exception as e:
+                raise
+                print("Can't handle message %r (%r)" % (data, e))
+
+            if payload["cmd"] == 'iam':
+                cmd = payload["cmd"]
+
+                if cmd == 'iam' and payload["sid"] not in self.nodes:
+                    print addr, ": ", payload
+                    #self.gateways.add(payload["ip"])
+                    self.gnodes[payload["sid"]] = dict(model="gateway")
+                    self.gnodes[payload["sid"]]['ip'] = payload["ip"]
+                    #self.request_sids(payload["sid"])
+
+
+        for sid in self.gnodes:
+            gateway_ip = self.gnodes[sid]['ip']
+            self.request_sids(payload["sid"], gateway_ip)
+
+            endTime = datetime.datetime.now() + datetime.timedelta(seconds=2)
+            print "Waiting for replies (2s) ..."
+            while datetime.datetime.now() <= endTime:
+                data, addr = self.socket.recvfrom(self.SOCKET_BUFSIZE)
+                try:
+                    payload = json.loads(data.decode("utf-8"))
+                    #print addr, ": ", payload
+                except Exception as e:
+                    raise
+                    print("Can't handle message %r (%r)" % (data, e))
+
+                cmd = payload["cmd"]
+                if cmd == "get_id_list_ack" and payload["sid"] == sid:
+
+                    device_sids = json.loads(payload["data"])
+
+                    if payload["sid"] not in self.gnodes:
+                        pass
+                        #self.nodes[payload["sid"]] = dict(model=u'gateway')
+                    else:
+                        self.gnodes[payload["sid"]]["nodes"] = device_sids
+
+                    if "token" in payload:
+                        self.last_tokens[payload["sid"]] = payload['token']
+
+                    print addr, ": ", payload
+                    print "NODES:", self.gnodes
+                    break
+
+        for sid in self.gnodes:
+            if self.gnodes[sid]['model'] == 'gateway':
+                for sub_node in self.gnodes[sid]['nodes']:
+                    #print sid, self.nodes[sid]['ip']
+                    self.request_current_status(sub_node, self.gnodes[sid]['ip'])
+
+                    endTime = datetime.datetime.now() + datetime.timedelta(seconds=2)
+                    #print "Waiting for reply from ", sub_node
+                    while datetime.datetime.now() <= endTime:
+                        data, addr = self.socket.recvfrom(self.SOCKET_BUFSIZE)
+                        try:
+                            payload = json.loads(data.decode("utf-8"))
+                            #print addr, ": ", payload
+                        except Exception as e:
+                            raise
+                            print("Can't handle message %r (%r)" % (data, e))
+
+                        cmd = payload["cmd"]
+
+                        if cmd == "read_ack" and payload["sid"] not in self.nodes and sub_node == payload["sid"]:
+                            try:
+                                self.nodes[payload["sid"]] = dict(model=payload["model"])
+                                self.nodes[payload["sid"]]["gateway_address"] = self.gnodes[sid]['ip']
+                                self.nodes[payload["sid"]]["gateway_sid"] = sid
+                            except KeyError, e:
+                                print "KeyError", e
+                            print addr, ": ", payload
+                            print "NODES:", len(self.nodes), self.nodes
+                            break
+        self.nodes.update(self.gnodes)
+        #print "Final:", len(self.nodes)
+        #for i in self.nodes:
+        #    print  i, self.nodes[i]
